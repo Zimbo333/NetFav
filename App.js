0346e0af4b79c52c54c37722f7726c7b36924b12
Zimbo333 <thanawat..kemwat@gmail.com>
2020-10-21 00:37:21 +0700

    add more firebase

--- App.js
+++ App.js
@@ -1,24 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-import NetFavNavigator from './navigator/NetFavNavigator';
+import NetFavNavigator from "./navigator/NetFavNavigator";
+import { db } from "./fb-firestore/firestore";
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text>hi dude</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-    <NetFavNavigator/>
-  );
+      <NetFavNavigator />
+      );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#45524b',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#45524b",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ fb-firestore/firestore.js
@@ -0,0 +1,18 @@
+import * as firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDH98zM1Y6TIf_qbY_IC0um0WfZ36Y1d5Q",
+  authDomain: "netfav-fa43e.firebaseapp.com",
+  databaseURL: "https://netfav-fa43e.firebaseio.com",
+  projectId: "netfav-fa43e",
+  storageBucket: "netfav-fa43e.appspot.com",
+  messagingSenderId: "762652878476",
+  appId: "1:762652878476:web:78dd490bb4936307007da9",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const dbh = firebase.firestore();
+
+export const db = dbh.collection('movie');
