08f1915e433a8dc81ddf6c5ea2b6c7b1cfe9586e
Tin Tosanguan <42942811+tinto88@users.noreply.github.com>
2020-10-21 01:14:26 +0700

    Update Login Screen

--- navigator/NetFavNavigator.js
+++ navigator/NetFavNavigator.js
@@ -3,9 +3,11 @@ import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 
 import HomeScreen from "../screens/HomeScreen";
+import LoginScreen from "../screens/LoginScreen";
 
 const NetFavNavigator = createStackNavigator(
   {
+    Login: LoginScreen,
     Home: HomeScreen,
 
   },
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -8,24 +8,69 @@ import {
   TouchableOpacity,
   Platform,
   TextInput,
+  Linking,
 } from "react-native";
 import Component from "react"
 
 const LoginScreen = (props) => {
-    return(
-      <view style={{flexDirection: "column"}}>
-        <view>
-            <Text>Login</Text>
-            <TextInput style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1 }} placeholder="username"/>
-            <TextInput style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1 }} placeholder="password"/>
-            <Button title="Login" color="red"/>
-        </view>
-      </view>
-    )
+  return (
+    <View style={styles.screen}>
+      <View style={styles.container}>
+        <Text style={styles.headerText}>Login</Text>
+        <TextInput style={styles.textinput} placeholder="username" />
+        <TextInput style={styles.textinput} placeholder="password" />
+        <Text
+          style={styles.fgpassword}
+          onPress={() => Linking.openURL('http://google.com')}>
+          Forgotten password?
+          </Text>
+        <Button title="Login" color="red" style={styles.button} />
+      </View>
+    </View>
+  )
 };
 
 LoginScreen.navigationOptions = {
-    headerTitle: "NetFav",
+  headerTitle: "NetFav",
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: '#45524b',
+    // justifyContent: "center",
+    // alignItems: "stretch",
+  },
+  container: {
+    padding: 10,
+    alignItems: "stretch",
+    flex: 1,
+    // backgroundColor: 'blue',
+    margin: 10,
+  },
+  textinput: {
+    margin: 10,
+    padding: 10,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    backgroundColor: 'white',
+  },
+  headerText: {
+    color: 'white',
+    fontSize: '250%',
+    fontWeight: 'bold',
+    alignSelf: "center"
+  },
+  fgpassword: {
+    marginBottom: 10,
+    marginHorizontal: 10,
+    color: 'white',
+    textDecorationLine: 'underline'
+  },
+  button: {
+
+  }
+});
 
 export default LoginScreen;
