85ff045d9126eee0caeb1fa7c55d87caba82de44
Tin Tosanguan <42942811+tinto88@users.noreply.github.com>
2020-11-18 13:00:24 +0700

    Update Tab Navi

--- navigator/NetFavNavigator.js
+++ navigator/NetFavNavigator.js
@@ -1,22 +1,69 @@
 // import คอมโพเนนต์ที่จำเป็น
+import React from "react";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import HomeScreen from "../screens/HomeScreen";
 import LoginScreen from "../screens/LoginScreen";
 
+const NetFavTabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons
+              name="ios-restaurant"
+              size={24}
+              color={tabInfo.tintColor}
+            />
+          );
+        },
+      },
+    },
+    Firstpage: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons
+              name="ios-restaurant"
+              size={24}
+              color={tabInfo.tintColor}
+            />
+          );
+        },
+      },
+    },
+    // Favorites: {
+    //   screen: FavNavigator,
+    //   navigationOptions: {
+    //     tabBarIcon: (tabInfo) => {
+    //       return (<Ionicons name="ios-star" size={24} color={tabInfo.tintColor} />);
+    //     },
+    //     tabBarLabel: "My Favorites",
+    //   }
+    // }
+  },
+  {
+    tabBarOptions: { activeTintColor: "orange" },
+  }
+);
+
 const NetFavNavigator = createStackNavigator(
   {
     Login: LoginScreen,
-    Home: HomeScreen,
-
+    Home: NetFavTabNavigator,
   },
   {
     defaultNavigationOptions: {
-      headerStyle: { backgroundColor: "#232926", },
+      headerStyle: { backgroundColor: "#232926" },
       headerTintColor: "red",
       headerTitleStyle: {
-        fontWeight: 'bold',
+        fontWeight: "bold",
       },
     },
   }
